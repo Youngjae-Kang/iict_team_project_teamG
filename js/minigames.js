fb5517278433d6a2f0a5d81cb1a5279f76053c54
Youngjae-Kang <141853811+Youngjae-Kang@users.noreply.github.com>
2025-12-15 21:49:49 +0900

    revise player color

--- js/minigames.js
+++ js/minigames.js
@@ -238,7 +238,7 @@ function playCrossy() {
     if(dist(mgCrossy.player.x,mgCrossy.player.y,car.x,car.y)<40) 
     { finishMinigame(false); return; }
   }
-  fill(100,100,255); ellipse(mgCrossy.player.x,mgCrossy.player.y,30,30);
+  fill(0); stroke(0); ellipse(mgCrossy.player.x,mgCrossy.player.y,30,30);
  if(mgCrossy.player.y<100 && mgCrossy.player.x>400 && mgCrossy.player.x<560) finishMinigame(true);
   mgTimer--; drawTimerBar();
   if(mgTimer<=0) finishMinigame(false);
